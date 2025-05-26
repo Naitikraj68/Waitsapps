54367c6902c927cbd150ba7b3191bd4ad3f633f6
Naitikraj68 <money58322@gmail.com>
2025-05-26 18:28:05 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,7 +6,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyBo_Ug-v80QdFcwgJY5Q6ggr9VSVyoH3SU",
   authDomain: "dosti-6950f.firebaseapp.com",
   projectId: "dosti-6950f",
-  storageBucket: "dosti-6950f.firebasestorage.app"
+  storageBucket: "dosti-6950f.firebasestorage.app",
+  messagingSenderId: "493956063524",
+  appId: "1:493956063524:web:83e2cf8af5c07ef8fd13b1"
 };
 
 const app = initializeApp(firebaseConfig);
@@ -14,4 +16,4 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { db, auth, provider };￼Enter
+export { db, auth, provider };
